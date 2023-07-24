4e5bc5dac09516edf154ce7fdf8280ae03ea60bb
Carlos Martinez <cmartinez@mcontigo.com>
2023-07-24 17:39:45 -0300

    Update/WPI: Comments

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { App } from "./App";
 import ReactDOM from "react-dom/client";
 
-// Main
+// Root
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<App />
